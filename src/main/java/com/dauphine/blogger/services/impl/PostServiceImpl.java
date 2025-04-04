b52cde56a204b5ac22d2f86e2aec32341a96cc38
MenSouff <mendel.souffir07@gmail.com>
2025-04-04 16:53:50 +0200

    add `post` repository layer with JPA

--- src/main/java/com/dauphine/blogger/services/impl/PostServiceImpl.java
+++ src/main/java/com/dauphine/blogger/services/impl/PostServiceImpl.java
@@ -4,60 +4,53 @@ import org.springframework.stereotype.Service;
 
 import com.dauphine.blogger.models.Category;
 import com.dauphine.blogger.models.Post;
+import com.dauphine.blogger.repositories.PostRepository;
 import com.dauphine.blogger.services.PostService;
 
-import java.util.ArrayList;
 import java.util.List;
 import java.util.UUID;
 
 @Service
 public class PostServiceImpl implements PostService {
 
-    private final List<Post> posts;
+    private final PostRepository repository;
 
-    public PostServiceImpl() {
-        this.posts = new ArrayList<>();
-        posts.add(new Post("Hello Blog", "Welcome to my new blog!", new Category(UUID.randomUUID(), "my 1st category")));
-        posts.add(new Post("Second Post", "This is my second post with more content.", new Category(UUID.randomUUID(), "my 2nd category")));
-        posts.add(new Post("Tech Talk", "Let's discuss Java and Spring Boot!", new Category(UUID.randomUUID(), "my 3th category")));
+    public PostServiceImpl(PostRepository repository) {
+        this.repository = repository;
     }
 
     @Override
     public List<Post> getAll() {
-        return posts;
+        return repository.findAll();
     }
 
     @Override
     public Post getById(UUID id) {
-        return posts.stream()
-                .filter(p -> p.getId().equals(id))
-                .findFirst()
-                .orElse(null);
+        return repository.findById(id)
+                        .orElse(null);
     }
 
     @Override
     public Post create(String title, String content, Category category) {
         Post post = new Post(title, content, category);
-        posts.add(post);
-        return post;
+        return repository.save(post);
     }
 
     @Override
     public Post update(UUID id, String title, String content, Category category) {
-        Post post = posts.stream()
-                        .filter(p -> id.equals(p.getId()))
-                        .findFirst()
-                        .orElse(null);
+        Post post = getById(id);
         if (post != null) {
             post.setTitle(title);
             post.setContent(content);
             post.setCategory(category);
+            return repository.save(post);
         }
-        return post;
+        return null;
     }
 
     @Override
     public boolean delete(UUID id) {
-        return posts.removeIf(p -> p.getId().equals(id));
+        repository.deleteById(id);
+        return true;
     }
 }
